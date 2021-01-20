c0f31737bf5a3a77922234d18881513c32d2d114
Igor Santos <igornzds@gmail.com>
2021-01-20 16:20:01 -0300

    Finalizar Modal

--- src/api.js
+++ src/api.js
@@ -70,3 +70,12 @@ export function PHOTOS_GET({ page, total, user }) {
     },
   };
 }
+
+export function PHOTO_GET(id) {
+  return {
+    url: `${API_URL}/api/photo/${id}`,
+    options: {
+      cache: 'no-store',
+    },
+  };
+}
