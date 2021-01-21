979cda1fd42ee7f452db46b2043362f95144bc25
Igor Santos <igornzds@gmail.com>
2021-01-21 14:37:56 -0300

    Criar página de Foto (single)

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -14,7 +14,10 @@ const PhotoComments = (props) => {
 
   return (
     <>
-      <ul ref={commentsSection} className={styles.comments}>
+      <ul
+        ref={commentsSection}
+        className={`${styles.comments} ${props.single ? styles.single : ''}`}
+      >
         {comments &&
           comments.map((comment) => (
             <li key={comment.comment_ID}>
@@ -23,7 +26,13 @@ const PhotoComments = (props) => {
             </li>
           ))}
       </ul>
-      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+      {login && (
+        <PhotoCommentsForm
+          single={props.single}
+          id={props.id}
+          setComments={setComments}
+        />
+      )}
     </>
   );
 };
